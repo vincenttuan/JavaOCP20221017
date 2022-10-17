dfaed85cd6112ab289b817d0b49897547c661b56
teacher <teacher@BE-403->
2022-10-17 21:46:15 +0800

    ocp

--- src/main/java/com/ocp/day1/CalcMath.java
+++ src/main/java/com/ocp/day1/CalcMath.java
@@ -5,11 +5,10 @@ public class CalcMath {
         // 桃園三結義學生時代成績 100, 95, 80
         // 計算總分與平均
         int x = 100, y = 95, z = 80;
-        int sum = x + y + z;
-        double avg = sum / 3.0;
-        double dx = 1/3.0 * (Math.pow(x-avg, 2) + Math.pow(y-avg, 2) + Math.pow(z-avg, 2));
-        double sd = Math.sqrt(dx); // 標準差: Math.sqrt 開根號
-        double cv = sd / avg; // 變異係數
+        int    sum = calcSum(x, y, z);
+        double avg = calcAvg(x, y, z);
+        double sd  = calcSD(x, y, z);
+        double cv  = calcCV(x, y, z);
         System.out.printf("總分: %d, 平均: %.1f 標準差: %.1f, 變異係數: %.3f\n", sum, avg, sd, cv);
     }
     // 將方法拆分成 計算總分的方法, 計算平均的方法, 計算標準差的方法, 計算變異係數的方法
