c1dd7db955352b2b18a8bcdc073022ae5bb7c6c0
teacher <teacher@BE-403->
2022-10-21 19:15:55 +0800

    ocp

--- src/main/java/com/ocp/day2/bar/Apple.java
+++ src/main/java/com/ocp/day2/bar/Apple.java
@@ -1,5 +1,15 @@
 package com.ocp.day2.bar;
 
 public class Apple {
-    
+    public static void a1() {
+        System.out.println("a1()");
+    }
+    static void a2() {
+        System.out.println("a2()");
+    }
+    private static void a3() {
+        System.out.println("a3()");
+        a1();
+        a2();
+    }
 }
--- src/main/java/com/ocp/day2/foo/Cherry.java
+++ src/main/java/com/ocp/day2/foo/Cherry.java
@@ -1,5 +1,9 @@
 package com.ocp.day2.foo;
 
+import com.ocp.day2.bar.Apple;
+
 public class Cherry {
-    
+    public static void cici() {
+        Apple.a1();
+    }
 }
