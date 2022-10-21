c32be685397b6a501e7caec50d38acbf15bfc95c
teacher <teacher@BE-403->
2022-10-21 20:32:56 +0800

    ocp

---
+++ src/main/java/com/ocp/day2/ArrayDemo2.java
@@ -0,0 +1,13 @@
+package com.ocp.day2;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        // 陣列自訂初始資料
+        //int[] scores = new int[]{100, 92, 85, 73, 68}; // part 1
+        int[] scores = {100, 92, 85, 73, 68}; // part 2
+        // 直接印出 scores
+        System.out.println(scores); // [I@762efe5d
+                                    // [ 一維陣列, I 表示 int, @ 在, 762efe5d 就是物件的16進位雜湊值(hash code)
+        
+    }
+}
