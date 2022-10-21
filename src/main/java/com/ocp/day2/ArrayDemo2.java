05cf6733cb70b0b080d9e71fa0a9dae4a963545e
teacher <teacher@BE-403->
2022-10-21 20:51:40 +0800

    ocp

--- src/main/java/com/ocp/day2/ArrayDemo2.java
+++ src/main/java/com/ocp/day2/ArrayDemo2.java
@@ -10,22 +10,26 @@ public class ArrayDemo2 {
         // 直接印出 scores
         System.out.println(scores); // [I@762efe5d
                                     // [ 一維陣列, I 表示 int, @ 在, 762efe5d 就是物件的16進位雜湊值(hash code)
-        // 印出每一個元素內容使用 for-loop
+        // 1. 印出每一個元素內容使用 for-loop
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + ", ");
         }
         System.out.println(); // 換行符號
-        // 印出每一個元素內容使用 for-each(for/in)
+        // 2. 印出每一個元素內容使用 for-each(for/in)
         for(int score : scores) {
             System.out.print(score + ", ");
         }
         System.out.println(); // 換行符號
-        // 印出每一個元素內容使用 for-loop (加強版)
+        // 3. 印出每一個元素內容使用 for-loop (加強版)
         for (int i = 0, len = scores.length ; i < len; i++) {
             System.out.print(scores[i] + ", ");
         }
         System.out.println(); // 換行符號
-        // 印出每一個元素內容使用 Arrays 類別所提供的 toString() 方法
+        // 4. 印出每一個元素內容使用 Arrays 類別所提供的 toString() 方法
         System.out.println(Arrays.toString(scores));
+        // 5. 印出每一個元素內容使用 Java 8 Stream 串流
+        Arrays.stream(scores).forEach(score -> System.out.print(score + ", "));
+        System.out.println(); // 換行符號
+        
     }
 }
