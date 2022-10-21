92c771b29f4db187514797cffea8e5c1452748ea
teacher <teacher@BE-403->
2022-10-21 21:37:19 +0800

    ocp

--- src/main/java/com/ocp/day2/ArrayDemo4.java
+++ src/main/java/com/ocp/day2/ArrayDemo4.java
@@ -1,6 +1,7 @@
 package com.ocp.day2;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class ArrayDemo4 {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class ArrayDemo4 {
         // 計算個數, 總和, 平均, 最大值, 最小值
         // 使用 Java Stream 來解決
         // 因為串流Stream用完之後不得重複使用所以每一次都要重新建立串流Stream
+        // 不然會拋出 stream has already been operated upon or closed
         System.out.printf("個數: %d\n", Arrays.stream(scores).count());
         System.out.printf("總和: %d\n", Arrays.stream(scores).sum());
         System.out.printf("平均: %.1f\n", Arrays.stream(scores).average().getAsDouble());
