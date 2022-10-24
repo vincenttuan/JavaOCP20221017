fcd1476976642819d6f297060a3cd6a205e0eeef
teacher <teacher@BE-403->
2022-10-24 21:44:07 +0800

    ocp

--- src/main/java/com/ocp/day3/StringArrayDemo4.java
+++ src/main/java/com/ocp/day3/StringArrayDemo4.java
@@ -1,5 +1,7 @@
 package com.ocp.day3;
 
+import java.util.Arrays;
+
 public class StringArrayDemo4 {
     public static void main(String[] args) {
         String str = "90, 85, 35, 53, 76";
@@ -8,7 +10,12 @@ public class StringArrayDemo4 {
         // int x = Integer.parseInt("90"); // 將字串 "90" 轉成數字 90
         String[] scores = str.split(", "); // ["90", "85", "35", "53", "76"]
         // Java 8
-        
+        double avg = Arrays.stream(scores)
+                           .mapToInt(score -> Integer.parseInt(score)) // .mapToInt(Integer::parseInt)
+                           .filter(score -> score >= 60)
+                           .average()
+                           .getAsDouble();
+        System.out.printf("avg = %.1f\n", avg);
         
     }
 }
