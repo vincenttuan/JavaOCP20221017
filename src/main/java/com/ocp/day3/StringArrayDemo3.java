ca89ac105435f8b816ef9bdf8bfbe32e4f27c7a3
teacher <teacher@BE-403->
2022-10-24 21:19:10 +0800

    ocp

--- src/main/java/com/ocp/day3/StringArrayDemo3.java
+++ src/main/java/com/ocp/day3/StringArrayDemo3.java
@@ -5,5 +5,9 @@ public class StringArrayDemo3 {
         String str = "90, 85, 35, 53, 76";
         // 請計算及格人數的平均分數 ?
         
+        // 字串轉數字
+        int x = Integer.parseInt("90"); // 將字串 "90" 轉成數字 90
+        
+        
     }
 }
