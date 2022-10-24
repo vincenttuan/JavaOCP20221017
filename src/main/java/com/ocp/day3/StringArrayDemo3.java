4af07a6f52fccc2eee38b34cdfe9ab037723b88c
teacher <teacher@BE-403->
2022-10-24 21:17:33 +0800

    ocp

---
+++ src/main/java/com/ocp/day3/StringArrayDemo3.java
@@ -0,0 +1,9 @@
+package com.ocp.day3;
+
+public class StringArrayDemo3 {
+    public static void main(String[] args) {
+        String str = "90, 85, 35, 53, 76";
+        // 請計算及格人數的平均分數 ?
+        
+    }
+}
