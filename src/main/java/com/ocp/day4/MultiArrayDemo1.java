5d3e2a99d0b115133c63c6c151781f85808ed4f9
teacher <teacher@BE-403->
2022-10-28 19:19:14 +0800

    ocp

--- src/main/java/com/ocp/day4/MultiArrayDemo1.java
+++ src/main/java/com/ocp/day4/MultiArrayDemo1.java
@@ -6,9 +6,9 @@ public class MultiArrayDemo1 {
         System.out.printf("x.length = %d\n", x.length);
         System.out.printf("x[0].length = %d\n", x[0].length);
         System.out.printf("x[1].length = %d\n", x[1].length);
-        System.out.println(x);
-        System.out.println(x[0]);
-        System.out.println(x[1]);
+        System.out.println(x); // [[I@1888ff2c
+        System.out.println(x[0]); // [I@35851384
+        System.out.println(x[1]); // [I@649d209a
         // 印出二維陣列資料 ?
         System.out.println("直接印出:");
         System.out.println(x[0][0]);
@@ -18,6 +18,13 @@ public class MultiArrayDemo1 {
         System.out.println(x[1][1]);
         System.out.println(x[1][2]);
         System.out.println("利用 for-loop 印出:");
+        for(int i=0;i<x.length;i++) {
+            System.out.println(x[i]);
+            // 分析一維陣列 x[i]
+            for(int k=0;k<x[i].length;k++) {
+                System.out.println(x[i][k]);
+            }
+        }
         
     }
 }
