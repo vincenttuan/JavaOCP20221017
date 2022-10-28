560e8d90e6ba4665e2a7d898999cd0f41b7b5125
teacher <teacher@BE-403->
2022-10-28 19:36:35 +0800

    ocp

--- src/main/java/com/ocp/day4/MultiArrayDemo2.java
+++ src/main/java/com/ocp/day4/MultiArrayDemo2.java
@@ -5,10 +5,18 @@ public class MultiArrayDemo2 {
         // 二維陣列給定初始值
         int[][] x = {{100, 90, 80}, {70, 60, 50}};
         // 1. 請把所有內容印出
-        
-        
+        print(x);
         // 2. 計算全部加總的值
         
-        
     }
+    
+    // 印出二維陣列內容的方法
+    private static void print(int[][] arrays) {
+        for(int[] array : arrays) {
+            for(int value : array) {
+                System.out.println(value);
+            }
+        }
+    }
+    
 }
