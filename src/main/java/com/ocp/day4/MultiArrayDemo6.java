7e3a5a226af727beb1bd61a49ef5e606c2f261aa
teacher <teacher@BE-403->
2022-10-28 21:40:00 +0800

    ocp

--- src/main/java/com/ocp/day4/MultiArrayDemo6.java
+++ src/main/java/com/ocp/day4/MultiArrayDemo6.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MultiArrayDemo6 {
     public static void main(String[] args) {
-        // 3 個學生[國文, 數學]的成績如下
+        // A 班有 3 個學生[國文, 數學]的成績如下
         int[][] scores = {{100, 90}, {85, 70}, {95, 40}};
         // 所有成績的加總 = ?
         // 利用 Java stream 來進行分析
@@ -13,5 +13,11 @@ public class MultiArrayDemo6 {
                         .flatMapToInt(array -> Arrays.stream(array)) // 一維陣列串流
                         .sum();
         System.out.println(sum);
+        // A 班的班平均是多少 ? 將 3 個學生的每一個人平均算出來之後再加總除以 3
+        // {{100, 90}, {85, 70}, {95, 40}} -> { 95.0, 77.5, 67.5 } -> 80.0
+        double avg = Arrays.stream(scores) // {{100, 90}, {85, 70}, {95, 40}}
+                           .mapToDouble(score -> Arrays.stream(score).average().getAsDouble()) // { 95.0, 77.5, 67.5 }
+                           .average().getAsDouble(); // 80.0
+        System.out.println(avg);
     }
 }
