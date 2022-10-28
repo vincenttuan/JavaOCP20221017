897b386a4cffd1f4f5053eec623d2f491e7f4843
teacher <teacher@BE-403->
2022-10-28 19:28:59 +0800

    ocp

---
+++ src/main/java/com/ocp/day4/MultiArrayDemo2.java
@@ -0,0 +1,14 @@
+package com.ocp.day4;
+
+public class MultiArrayDemo2 {
+    public static void main(String[] args) {
+        // 二維陣列給定初始值
+        int[][] x = {{100, 90, 80}, {70, 60, 50}};
+        // 1. 請把所有內容印出
+        
+        
+        // 2. 計算全部加總的值
+        
+        
+    }
+}
