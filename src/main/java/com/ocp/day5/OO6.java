b9d037be7d044212a42c6360085b7156655b6048
teacher <teacher@BE-403->
2022-10-31 19:57:20 +0800

    ocp

--- src/main/java/com/ocp/day5/OO6.java
+++ src/main/java/com/ocp/day5/OO6.java
@@ -1,5 +1,8 @@
 package com.ocp.day5;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class OO6 {
     public static void main(String[] args) {
         Student[] students = {
@@ -18,5 +21,11 @@ public class OO6 {
         for(Student student : students) {
             student.printStudentData();
         }
+        // Java Stream
+        Arrays.stream(students)
+                .forEach(student -> student.printStudentData());
+        // Java Stream + :: (方法參考)
+        Arrays.stream(students)
+                .forEach(Student::printStudentData);
     }
 }
