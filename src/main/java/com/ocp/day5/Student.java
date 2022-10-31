831f81b519608be0dbe7167ba1b1e3ae997c4998
teacher <teacher@BE-403->
2022-10-31 19:10:21 +0800

    ocp

--- src/main/java/com/ocp/day5/Student.java
+++ src/main/java/com/ocp/day5/Student.java
@@ -1,6 +1,11 @@
 package com.ocp.day5;
 
 public class Student {
+    // 物件變數
     String name;
     int score;
+    // 空實作建構子
+    public Student() {
+        System.out.println("建立 Student 物件");
+    }
 }
