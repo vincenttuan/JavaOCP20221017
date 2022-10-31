db0cd6fa6dbc8afac2740790764f6c7c8dd30498
teacher <teacher@BE-403->
2022-10-31 21:24:17 +0800

    ocp

--- src/main/java/com/ocp/day5/Account.java
+++ src/main/java/com/ocp/day5/Account.java
@@ -3,11 +3,18 @@ package com.ocp.day5;
 // 帳戶
 public class Account {
     String name; // 戶名
-    int balance; // 帳戶餘額
+    private int balance; // 帳戶餘額
     
     public Account(String name, int balance) {
         this.name = name;
         this.balance = balance;
+    }
+    
+    // 存款
+    public void deposit(int amount) {
+        if(amount > 0) { // 過濾 amount > 0
+            balance += amount;
+        }
     }
     
     public void printAccount() {
--- src/main/java/com/ocp/day5/OO9.java
+++ src/main/java/com/ocp/day5/OO9.java
@@ -4,5 +4,8 @@ public class OO9 {
     public static void main(String[] args) {
         Account ac1 = new Account("John", 10000);
         ac1.printAccount();
+        // 存款 $500
+        ac1.deposit(500);
+        ac1.printAccount();
     }
 }
