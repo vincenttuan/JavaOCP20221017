060e1c48cc7f945179019704a012c6cf5678b65f
teacher <teacher@BE-403->
2022-10-31 19:41:52 +0800

    ocp

---
+++ src/main/java/com/ocp/day5/OO6.java
@@ -0,0 +1,14 @@
+package com.ocp.day5;
+
+public class OO6 {
+    public static void main(String[] args) {
+        Student[] students = {
+            new Student("John", 80),
+            new Student("Mary", 40),
+            new Student("Bobo", 90),
+            new Student("Jack", 70),
+            new Student("Kita", 50),
+        };
+        System.out.println(students);
+    }
+}
