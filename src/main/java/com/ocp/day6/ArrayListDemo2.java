f081bafcce05ab2ce12cfde4271390f976ce87bf
teacher <teacher@BE-403->
2022-11-04 19:38:53 +0800

    ocp

--- src/main/java/com/ocp/day6/ArrayListDemo2.java
+++ src/main/java/com/ocp/day6/ArrayListDemo2.java
@@ -1,5 +1,7 @@
 package com.ocp.day6;
 
+import java.util.Scanner;
+
 public class ArrayListDemo2 {
     // 程式進入點
     public static void main(String[] args) {
@@ -8,7 +10,39 @@ public class ArrayListDemo2 {
     
     // 系統功能目錄
     public static void menu() {
-        
+        System.out.println("簽到系統");
+        System.out.println("----------------");
+        System.out.println("1. 簽到");
+        System.out.println("2. 修改");
+        System.out.println("3. 刪除");
+        System.out.println("4. 單筆查詢");
+        System.out.println("5. 全部查詢");
+        System.out.println("0. Exit");
+        System.out.println("----------------");
+        System.out.print("請選擇: ");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt(); // 得到使用者所輸入的選項
+        switch (choice) {
+            case 1:
+                add();
+                break;
+            case 2:
+                update();
+                break;
+            case 3:
+                delete();
+                break;
+            case 4:
+                findOne();
+                break;
+            case 5:
+                findAll();
+                break;
+            case 0:
+                exit();
+                break;
+            
+        }
     }
     
     // 簽到
