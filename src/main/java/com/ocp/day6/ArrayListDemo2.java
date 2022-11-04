6d5afe36474647ac9e4a94d5362f1cdbd01b5bcd
teacher <teacher@BE-403->
2022-11-04 19:28:06 +0800

    ocp

--- src/main/java/com/ocp/day6/ArrayListDemo2.java
+++ src/main/java/com/ocp/day6/ArrayListDemo2.java
@@ -1,7 +1,46 @@
 package com.ocp.day6;
 
 public class ArrayListDemo2 {
+    // 程式進入點
     public static void main(String[] args) {
         
     }
+    
+    // 系統功能目錄
+    public static void menu() {
+        
+    }
+    
+    // 簽到
+    public static void add() {
+        
+    }
+    
+    // 修改
+    public static void update() {
+    
+    }
+    
+    // 刪除
+    public static void delete() {
+    
+    }
+    
+    // 單筆查詢
+    public static void findOne() {
+    
+    }
+    
+    // 全部查詢
+    public static void findAll() {
+    
+    }
+    
+    // 離開系統
+    public static void exit() {
+        // status: 0 表示結束當前正在運行的程式
+        // status: 1 表示非正常結束
+        System.exit(0); 
+    }
+    
 }
