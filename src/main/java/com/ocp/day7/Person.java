2ea6ff00429e1419c6c9fb967fba50e5b44e9087
teacher <teacher@BE-403->
2022-11-07 19:04:40 +0800

    ocp

--- src/main/java/com/ocp/day7/Person.java
+++ src/main/java/com/ocp/day7/Person.java
@@ -4,15 +4,6 @@ public class Person {
     private String name;
     private int age;
     
-    // 建構子封裝
-    public Person() {
-    }
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    
     // getter / setter (方法封裝)
     public String getName() {
         return name;
---
+++ src/main/java/com/ocp/day7/Student.java
@@ -0,0 +1,14 @@
+package com.ocp.day7;
+
+public class Student extends Person {
+    private int score;
+    
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    
+}
