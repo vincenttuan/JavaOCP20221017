744a423f40adc282039d899767475f228f15f422
teacher <teacher@BE-403->
2022-11-07 20:42:38 +0800

    ocp

--- src/main/java/com/ocp/day7_2/Main2.java
+++ src/main/java/com/ocp/day7_2/Main2.java
@@ -3,6 +3,14 @@ package com.ocp.day7_2;
 public class Main2 {
     public static void main(String[] args) {
         Classroom classroom = new Classroom(405);
-        
+        Teacher teacher = new Teacher("Helen", 30, 70000);
+        Student s1 = new Student("John", 18, 90);
+        Student s2 = new Student("Mary", 17, 75);
+        Student s3 = new Student("Anita", 20, 100);
+        classroom.setTeacher(teacher);
+        classroom.addStudent(s1);
+        classroom.addStudent(s2);
+        classroom.addStudent(s3);
+        System.out.println(classroom);
     }
 }
