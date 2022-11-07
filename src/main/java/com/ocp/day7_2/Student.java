c7264229e172cd140b1736854a07c9d511320f60
teacher <teacher@BE-403->
2022-11-07 19:45:26 +0800

    ocp

--- src/main/java/com/ocp/day7_2/Student.java
+++ src/main/java/com/ocp/day7_2/Student.java
@@ -2,8 +2,24 @@ package com.ocp.day7_2;
 
 public class Student extends Person {
     
-    public Student(String name, int age) {
+    private int score;
+    
+    public Student(String name, int age, int score) {
         super(name, age);
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "score=" + score + '}' + super.toString();
     }
     
 }
