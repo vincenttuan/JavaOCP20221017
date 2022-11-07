fc20cb4d8da9865ac6db08a0f94e8f32245f63a9
teacher <teacher@BE-403->
2022-11-07 19:43:04 +0800

    ocp

--- src/main/java/com/ocp/day7_2/Person.java
+++ src/main/java/com/ocp/day7_2/Person.java
@@ -3,7 +3,7 @@ package com.ocp.day7_2;
 public class Person {
     private String name;
     private int age;
-
+    
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
---
+++ src/main/java/com/ocp/day7_2/Student.java
@@ -0,0 +1,9 @@
+package com.ocp.day7_2;
+
+public class Student extends Person {
+    
+    public Student(String name, int age) {
+        super(name, age);
+    }
+    
+}
