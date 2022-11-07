1882980b225cff290961d480c80f07b209358dd4
teacher <teacher@BE-403->
2022-11-07 20:08:11 +0800

    ocp

---
+++ src/main/java/com/ocp/day7_2/Classroom.java
@@ -0,0 +1,13 @@
+package com.ocp.day7_2;
+
+import java.util.ArrayList;
+
+// 組合模式
+public class Classroom {
+    
+    private int id; // 教室編號
+    private Teacher teacher;
+    private ArrayList<Student> students = new ArrayList<>();
+    
+    
+}
