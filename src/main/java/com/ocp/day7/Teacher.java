f53d11d2263262e01b7fbd6c78db71939524a187
teacher <teacher@BE-403->
2022-11-07 19:05:41 +0800

    ocp

---
+++ src/main/java/com/ocp/day7/Teacher.java
@@ -0,0 +1,15 @@
+package com.ocp.day7;
+
+public class Teacher extends Person {
+    private int salary;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    
+    
+}
