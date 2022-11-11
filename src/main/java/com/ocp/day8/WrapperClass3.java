48ce222874317df26bab97ced0e33f3e6a11737d
teacher <teacher@BE-403->
2022-11-11 21:37:11 +0800

    ocp

--- src/main/java/com/ocp/day8/WrapperClass3.java
+++ src/main/java/com/ocp/day8/WrapperClass3.java
@@ -11,6 +11,12 @@ public class WrapperClass3 {
         System.out.println(sum);
         Integer[] nums2 = {80, 70, 60};
         // 利用 Arrays.stream() 計算 sum()
-        
+        int sum2 = Arrays.stream(nums2)
+                         //.mapToInt(n -> n.intValue())
+                         //.mapToInt(Integer::intValue)
+                         .mapToInt(n -> n) // auto-unboxing
+                         .sum();
+        System.out.println(sum2);
+        // boxed() 可以將 int 轉 Integer
     }
 }
