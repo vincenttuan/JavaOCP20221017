0c405aa00d9da3f4076090c719c212c901391b17
teacher <teacher@BE-403->
2022-11-14 20:56:36 +0800

    ocp

---
+++ src/main/java/com/ocp/day9_3/Test.java
@@ -0,0 +1,10 @@
+package com.ocp.day9_3;
+
+public class Test {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Tiger tiger = new Tiger();
+        cat.叫聲();
+        tiger.叫聲();
+    }
+}
