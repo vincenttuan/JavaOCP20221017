c5bacd805f2862779909b2ed6599e551fd72af4a
teacher <teacher@BE-403->
2022-11-14 20:54:51 +0800

    ocp

---
+++ src/main/java/com/ocp/day9_3/Cat.java
@@ -0,0 +1,7 @@
+package com.ocp.day9_3;
+
+public class Cat {
+    public void 叫聲() {
+        System.out.println("喵~~");
+    }
+}
---
+++ src/main/java/com/ocp/day9_3/Tiger.java
@@ -0,0 +1,8 @@
+package com.ocp.day9_3;
+
+public class Tiger extends Cat {
+    @Override
+    public void 叫聲() {
+        System.out.println("吼~~");
+    }
+}
