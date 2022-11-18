79fbb1e55ff48456dc1944f1da05195d98767534
teacher <teacher@BE-403->
2022-11-18 20:58:40 +0800

    ocp

--- src/main/java/com/ocp/day10_1/Airplane.java
+++ src/main/java/com/ocp/day10_1/Airplane.java
@@ -1,6 +1,7 @@
 package com.ocp.day10_1;
 
 public abstract class Airplane {
+    // 抽象類別中的抽象方法一定要加上 abstract
     public abstract void speed();
 }
  
--- src/main/java/com/ocp/day10_1/Car.java
+++ src/main/java/com/ocp/day10_1/Car.java
@@ -1,7 +1,7 @@
 package com.ocp.day10_1;
 
 public abstract class Car {
- 
-	public abstract void move();
+    // 抽象類別中的抽象方法一定要加上 abstract
+    public abstract void move();
 }
  
