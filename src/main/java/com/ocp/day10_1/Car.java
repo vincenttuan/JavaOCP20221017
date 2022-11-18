3701e1522e6d2034e6f013a8be154bb072956796
teacher <teacher@BE-403->
2022-11-18 21:14:14 +0800

    ocp

--- src/main/java/com/ocp/day10_1/Car.java
+++ src/main/java/com/ocp/day10_1/Car.java
@@ -1,6 +1,9 @@
 package com.ocp.day10_1;
 
 public abstract class Car {
+    public void printName() {
+        System.out.println("我是車");
+    }
     // 抽象類別中的抽象方法一定要加上 abstract
     public abstract void move();
 }
--- src/main/java/com/ocp/day10_1/Weapon.java
+++ src/main/java/com/ocp/day10_1/Weapon.java
@@ -1,6 +1,10 @@
 package com.ocp.day10_1;
 
 public interface Weapon {
+    // Java 8 之後可以利用 default 修飾方法, 該方法就可以有實作
+    public default void printName() {
+        System.out.println("我是武器");
+    }
     public abstract void shoot();
 }
  
