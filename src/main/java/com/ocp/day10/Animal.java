ec61b38711dc70bec2e58becf947451cca417c39
teacher <teacher@BE-403->
2022-11-18 19:13:40 +0800

    ocp

---
+++ src/main/java/com/ocp/day10/Animal.java
@@ -0,0 +1,7 @@
+package com.ocp.day10;
+
+public interface Animal {
+    String COPY_RIGHT = "ABC.com"; // public static final String COPY_RIGHT = "ABC.com";
+    void eat(); // public abstract void eat();
+    void play(); // public abstract void eat();
+}
