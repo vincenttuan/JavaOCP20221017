d35737b64727e2b2d19ead3f859093a2b7fe8fbf
teacher <teacher@BE-403->
2022-11-18 21:11:03 +0800

    ocp

--- src/main/java/com/ocp/day10_1/Drone.java
+++ src/main/java/com/ocp/day10_1/Drone.java
@@ -3,7 +3,7 @@ package com.ocp.day10_1;
 public class Drone extends Airplane {
     @Override
     public void speed() {
-        System.out.println("無人機入速度800km/h");
+        System.out.println("無人機速度800km/h");
     } 
 }
  
