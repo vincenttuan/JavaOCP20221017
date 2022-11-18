8ac2418c003308222d98e3181c9754c1abc380b9
teacher <teacher@BE-403->
2022-11-18 19:25:41 +0800

    ocp

---
+++ src/main/java/com/ocp/day10/PetStore.java
@@ -0,0 +1,12 @@
+package com.ocp.day10;
+
+public class PetStore {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        dog.eat();
+        dog.play();
+        cat.eat();
+        cat.play();
+    }
+}
