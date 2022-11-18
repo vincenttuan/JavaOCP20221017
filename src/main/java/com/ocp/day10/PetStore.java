b48ddc10a02b40919d015fb56c419016e3c7aee2
teacher <teacher@BE-403->
2022-11-18 19:30:16 +0800

    ocp

--- src/main/java/com/ocp/day10/PetStore.java
+++ src/main/java/com/ocp/day10/PetStore.java
@@ -1,12 +1,18 @@
 package com.ocp.day10;
 
+import java.util.ArrayList;
+
 public class PetStore {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
         dog.eat();
         dog.play();
         cat.eat();
         cat.play();
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(cat);
+        System.out.println(animals);
     }
 }
