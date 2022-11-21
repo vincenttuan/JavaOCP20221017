39bffc7ec6b6165be6b9a406c4149f9c47a2e58a
teacher <teacher@BE-403->
2022-11-21 18:57:27 +0800

    ocp

---
+++ src/main/java/com/ocp/day11/decorator/Bread.java
@@ -0,0 +1,20 @@
+package com.ocp.day11.decorator;
+
+public class Bread extends Food {
+    
+    public Bread() {
+        name = "麵包";
+        price = 40;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+    
+}
---
+++ src/main/java/com/ocp/day11/decorator/Food.java
@@ -0,0 +1,9 @@
+package com.ocp.day11.decorator;
+
+public abstract class Food {
+    protected String name;
+    protected int price;
+    
+    public abstract String getName();
+    public abstract int getPrice();
+}
