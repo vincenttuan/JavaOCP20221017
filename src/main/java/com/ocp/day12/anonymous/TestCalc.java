2b9c635bdd0eaa329d5da012402c04a20db01c15
teacher <teacher@BE-403->
2022-11-25 20:44:33 +0800

    ocp

--- src/main/java/com/ocp/day12/anonymous/TestCalc.java
+++ src/main/java/com/ocp/day12/anonymous/TestCalc.java
@@ -4,5 +4,16 @@ public class TestCalc {
     public static void main(String[] args) {
         Calc calc = new CalcImpl();
         System.out.println(calc.submit(10, 20));
+        // 匿名內部類別
+        Calc calc2 = new Calc(){
+            
+            @Override
+            public int submit(int x, int y) {
+                return x + y;
+            }
+        
+        };
+        System.out.println(calc2.submit(10, 20));
+        
     }
 }
