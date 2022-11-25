2142a5af17e77daacbe4db2ef67a611768a4bdb4
teacher <teacher@BE-403->
2022-11-25 19:09:13 +0800

    ocp

--- src/main/java/com/ocp/day12/innerclass/Employee.java
+++ src/main/java/com/ocp/day12/innerclass/Employee.java
@@ -3,11 +3,14 @@ package com.ocp.day12.innerclass;
 // 員工類別(外部類別)
 public class Employee {
     private String name; // 員工姓名
+    static int score = 100; // 外部類別可以定義類別成員
     public Employee(String name) {
         this.name = name;
     }
-    // 員工的工作類別(內部類別)
+    // 員工的工作類別(一般內部類別)
+    // 不可以有 static  類別成員(屬性/方法 )
     public class Job {
+        //static int score = 100; // 內部類別不可以定義類別成員
         private String name; // 工作名稱
         private int level; // 工作難易度(1~10)
         public Job(String name, int level) {
