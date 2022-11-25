a83967d8f39e4ef054adb80863dfab41203c6c12
teacher <teacher@BE-403->
2022-11-25 19:25:17 +0800

    ocp

--- src/main/java/com/ocp/day12/methodclass/Student.java
+++ src/main/java/com/ocp/day12/methodclass/Student.java
@@ -1,18 +1,18 @@
 package com.ocp.day12.methodclass;
 
 public class Student {
-    private String name;
+    private String name; // 學生姓名
     public Student(String name) {
         this.name = name;
     }
     
     public void printExam() {
-        String name = "Java"; // 考試科目
+        String subject = "Java"; // 考試科目
         class Exam {
-            int score = 90;
+            int score = 90; // 考試成績
             @Override
             public String toString() {
-                return name + ": " + score;
+                return name + ", " + subject + ", " + score;
             }
         }
         Exam exam = new Exam();
