5a64425fa9cc2c245430fcbd7129e187698bf65a
teacher <teacher@BE-403->
2022-11-28 19:41:05 +0800

    ocp

--- src/main/java/com/ocp/day13/FunctionDemo.java
+++ src/main/java/com/ocp/day13/FunctionDemo.java
@@ -1,5 +1,6 @@
 package com.ocp.day13;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class FunctionDemo {
@@ -18,5 +19,25 @@ public class FunctionDemo {
         Function<String, Integer> func2 = (words) -> words.split(" ").length;
         System.out.println(func2.apply("she sell sea shell on the sea shore"));
         
+        // BiFunction
+        /*
+            public interface BiFunction<T, U, R> {
+                R apply(T t, U u);
+            }
+        */
+        BiFunction<String, Integer, Double> biFunction = (name, value) -> {
+            double result = 0.0;
+            switch (name) {
+                case "正方形":
+                    result = Math.pow(value, 2);
+                    break;
+                case "圓形":
+                    result = Math.pow(value, 2) * Math.PI;
+                    break;
+            }
+            return result;
+        };
+        System.out.println("正方形面積: " + biFunction.apply("正方形", 10));
+        System.out.println("圓形面積: " + biFunction.apply("圓形", 10));
     }
 }
