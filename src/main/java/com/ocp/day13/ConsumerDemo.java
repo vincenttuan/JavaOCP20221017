c3ea432f269a2d4848bd485a3717c292bdf1701c
teacher <teacher@BE-403->
2022-11-28 19:18:12 +0800

    ocp

--- src/main/java/com/ocp/day13/ConsumerDemo.java
+++ src/main/java/com/ocp/day13/ConsumerDemo.java
@@ -3,15 +3,13 @@ package com.ocp.day13;
 import java.util.Arrays;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.ObjIntConsumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
         // Consumer
         Consumer<String> consumer = (String x) -> System.out.println("Hello " + x);
         consumer.accept("John");
-        // BiConsumer
-        BiConsumer<String, Integer> biConsumer = (name, age) -> System.out.println(name + " 的年齡是 " + age);
-        biConsumer.accept("John", 18);
         // Consumer 多參數
         Consumer<Integer[]> consumer2 = (values) -> {
             int sum = Arrays.stream(values).mapToInt(Integer::intValue).sum();
@@ -26,5 +24,21 @@ public class ConsumerDemo {
         };
         consumer3.accept(new int[]{70, 60, 50});
         
+        // BiConsumer
+        /*
+        public interface BiConsumer<T, U>
+            public void accept(T t, U u);
+        }
+        */
+        BiConsumer<String, Integer> biConsumer = (name, age) -> System.out.println(name + " 的年齡是 " + age);
+        biConsumer.accept("John", 18);
+        // ObjIntConsumer
+        /*
+        public interface ObjIntConsumer<T>
+            public void accept(T t, int x);
+        }
+        */
+        ObjIntConsumer<String> objIntConsumer = (name, age) -> System.out.println(name + " 的年齡是 " + age);
+        objIntConsumer.accept("John", 18);
     }
 }
