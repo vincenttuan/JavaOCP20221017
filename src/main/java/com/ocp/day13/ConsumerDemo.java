095a0bbc68ec84c31213df137751d1c897b175e0
teacher <teacher@BE-403->
2022-11-28 18:56:35 +0800

    ocp

--- src/main/java/com/ocp/day13/ConsumerDemo.java
+++ src/main/java/com/ocp/day13/ConsumerDemo.java
@@ -1,5 +1,7 @@
 package com.ocp.day13;
 
+import java.util.Arrays;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
@@ -7,5 +9,14 @@ public class ConsumerDemo {
         // Consumer
         Consumer<String> consumer = (String x) -> System.out.println("Hello " + x);
         consumer.accept("John");
+        // BiConsumer
+        BiConsumer<String, Integer> biConsumer = (name, age) -> System.out.println(name + " 的年齡是 " + age);
+        biConsumer.accept("John", 18);
+        // Consumer 多參數
+        Consumer<Integer[]> consumer2 = (values) -> {
+            int sum = Arrays.stream(values).mapToInt(Integer::intValue).sum();
+            System.out.println(sum);
+        };
+        consumer2.accept(new Integer[]{100, 90, 80});
     }
 }
