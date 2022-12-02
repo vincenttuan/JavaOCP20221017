00537effac53d0dbae26d3ed5b0eb506296ecc5c
teacher <teacher@BE-403->
2022-12-02 21:41:01 +0800

    ocp

---
+++ src/main/java/com/ocp/day14_set/HashSetDemo4.java
@@ -0,0 +1,19 @@
+package com.ocp.day14_set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HashSetDemo4 {
+    public static void main(String[] args) {
+        Set<Num> nums = new LinkedHashSet<>();
+        nums.add(new Num(100, 0.5));
+        nums.add(new Num(500, 0.2));
+        nums.add(new Num(1000, 0.1));
+        System.out.println(nums);
+        // 計算出每個元素 cash * rate 的總和
+        double sum = nums.stream()
+                         .mapToDouble(num -> num.getCash() * num.getRate())
+                         .sum();
+        System.out.println(sum);
+    }
+}
