341757ebd1e382ec7af2c2a1cec034dca059c11d
teacher <teacher@BE-403->
2022-12-02 20:41:21 +0800

    ocp

---
+++ src/main/java/com/ocp/day14_set/HashSetDemo2.java
@@ -0,0 +1,27 @@
+package com.ocp.day14_set;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HashSetDemo2 {
+    public static void main(String[] args) {
+        // 加入泛型 <> 限制
+        Set<Integer> scores = new LinkedHashSet<>();
+        scores.add(100);
+        scores.add(80);
+        scores.add(70);
+        System.out.println(scores);
+        // 計算總分
+        int sum = 0;
+        Iterator<Integer> iter = scores.iterator();
+        while (iter.hasNext()) {
+            sum += iter.next();
+        }
+        System.out.println(sum);
+        
+        int sum2 = scores.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum2);
+        
+    }
+}
