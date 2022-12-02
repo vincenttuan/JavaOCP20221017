ab80a4ec193ead2d645f31fd602865127243f12f
teacher <teacher@BE-403->
2022-12-02 21:07:29 +0800

    ocp

---
+++ src/main/java/com/ocp/day14_set/HashSetDemo3.java
@@ -0,0 +1,20 @@
+package com.ocp.day14_set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HashSetDemo3 {
+    public static void main(String[] args) {
+        Set<Set<Number>> values = new LinkedHashSet<>();
+        Set<Number> item1 = new LinkedHashSet<>();
+        Set<Number> item2 = new LinkedHashSet<>();
+        Set<Number> item3 = new LinkedHashSet<>();
+        item1.add(100);item1.add(0.5);
+        item2.add(500);item2.add(0.2);
+        item3.add(1000);item3.add(0.1);
+        values.add(item1);
+        values.add(item2);
+        values.add(item3);
+        System.out.println(values); // [[100, 0.5], [500, 0.2], [1000, 0.1]]
+    }
+}
