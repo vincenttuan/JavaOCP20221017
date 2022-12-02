3736b718cbeb7c42d98ef9c10755a37a39d8e707
teacher <teacher@BE-403->
2022-12-02 20:52:39 +0800

    ocp

--- src/main/java/com/ocp/day14_set/HashSetDemo2.java
+++ src/main/java/com/ocp/day14_set/HashSetDemo2.java
@@ -7,14 +7,14 @@ import java.util.Set;
 public class HashSetDemo2 {
     public static void main(String[] args) {
         // 加入泛型 <> 限制
-        Set<Integer> scores = new LinkedHashSet<>();
+        Set<Number> scores = new LinkedHashSet<>();
         scores.add(100);
         scores.add(null);
         scores.add(80);
         scores.add(70);
         System.out.println(scores);
         // 將 null 元素移除(remove)
-        Iterator<Integer> iter2 = scores.iterator();
+        Iterator<Number> iter2 = scores.iterator();
         while (iter2.hasNext()) {
             if(iter2.next() == null) {
                 iter2.remove();
@@ -23,13 +23,14 @@ public class HashSetDemo2 {
         System.out.println(scores);
         // 計算總分
         int sum = 0;
-        Iterator<Integer> iter = scores.iterator();
+        Iterator<Number> iter = scores.iterator();
         while (iter.hasNext()) {
-            sum += iter.next();
+            sum += iter.next().intValue();
         }
         System.out.println(sum);
         
-        int sum2 = scores.stream().mapToInt(Integer::intValue).sum();
+        //int sum2 = scores.stream().mapToInt(next -> next.intValue()).sum();
+        int sum2 = scores.stream().mapToInt(Number::intValue).sum();
         System.out.println(sum2);
         
     }
