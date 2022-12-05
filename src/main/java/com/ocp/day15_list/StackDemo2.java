ae7a9ddb6038f5f7e9826e2c60d97f16588baae7
teacher <teacher@BE-403->
2022-12-05 21:03:01 +0800

    ocp

---
+++ src/main/java/com/ocp/day15_list/StackDemo2.java
@@ -0,0 +1,12 @@
+package com.ocp.day15_list;
+
+/*
+Lab
+利用 java.util.Stack 做字串反轉
+說明 : 使用者輸入 "Java" 則會顯示 "avaJ"
+*/
+public class StackDemo2 {
+    public static void main(String[] args) {
+        
+    }
+}
