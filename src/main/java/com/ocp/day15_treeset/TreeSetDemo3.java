aafe17c114ae609b33b6b85dbf892520d4246a1d
teacher <teacher@BE-403->
2022-12-05 19:08:19 +0800

    ocp

---
+++ src/main/java/com/ocp/day15_treeset/TreeSetDemo3.java
@@ -0,0 +1,18 @@
+package com.ocp.day15_treeset;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo3 {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("John", 30, 45000);
+        Employee emp2 = new Employee("Mary", 25, 48000);
+        Employee emp3 = new Employee("Bob", 40, 55000);
+        Set<Employee> employees = new TreeSet<>();
+        employees.add(emp1);
+        employees.add(emp2);
+        employees.add(emp3);
+        System.out.println(employees);
+        
+    }
+}
