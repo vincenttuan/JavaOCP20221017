2df242abba647a9fdbeea74a52150b614dc25a9a
teacher <teacher@BE-403->
2022-12-05 21:18:23 +0800

    ocp

--- src/main/java/com/ocp/day15_list/StackDemo2.java
+++ src/main/java/com/ocp/day15_list/StackDemo2.java
@@ -1,5 +1,8 @@
 package com.ocp.day15_list;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /*
 Lab
 利用 java.util.Stack 做字串反轉
@@ -7,6 +10,17 @@ Lab
 */
 public class StackDemo2 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入文字: ");
+        String word = scanner.next();
+        char[] chars = word.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(Character c : chars) {
+            stack.push(c);
+        }
+        System.out.println(stack);
+        while (!stack.isEmpty()) {            
+            System.out.print(stack.pop());
+        }
     }
 }
