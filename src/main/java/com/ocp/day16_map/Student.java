35332ed548acdbd9c85ef3673d5951e9406539f5
teacher <teacher@BE-403->
2022-12-09 19:07:46 +0800

    ocp

---
+++ src/main/java/com/ocp/day16_map/Student.java
@@ -0,0 +1,36 @@
+package com.ocp.day16_map;
+
+public class Student {
+    private Integer id; // 學號
+    private String name; // 姓名
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+}
