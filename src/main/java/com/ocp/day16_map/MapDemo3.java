513380f86ffe51bc24e50c123a3b24ccc98e2c62
teacher <teacher@BE-403->
2022-12-09 19:28:16 +0800

    ocp

--- src/main/java/com/ocp/day16_map/MapDemo3.java
+++ src/main/java/com/ocp/day16_map/MapDemo3.java
@@ -1,5 +1,32 @@
 package com.ocp.day16_map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapDemo3 {
+    
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "John");
+        Student s2 = new Student(2, "Mary");
+        Student s3 = new Student(3, "Bob");
+        Exam e1 = new Exam("Java", 100);
+        Exam e2 = new Exam("Java", 80);
+        Exam e3 = new Exam("Java", 75);
+        
+        Map<Student, Exam> map = new HashMap<>();
+        map.put(s1, e1);
+        map.put(s2, e2);
+        map.put(s3, e3);
+        
+        System.out.println(map);
+        
+        int sum = map.entrySet()
+                     .stream()
+                     .mapToInt(entry -> entry.getValue().getScore())
+                     .sum();
+        System.out.println(sum);
+        
+        
+    }
     
 }
