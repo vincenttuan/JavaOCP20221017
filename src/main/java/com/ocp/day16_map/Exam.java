456d58b920d79906d53aae1c5b2220534555a465
teacher <teacher@BE-403->
2022-12-09 19:11:42 +0800

    ocp

--- src/main/java/com/ocp/day16_map/Exam.java
+++ src/main/java/com/ocp/day16_map/Exam.java
@@ -1,8 +1,10 @@
 package com.ocp.day16_map;
 
+import java.util.Objects;
+
 public class Exam {
     private String subject; // 科目
-    private Integer score;
+    private Integer score; // 分數
 
     public Exam() {
     }
@@ -28,6 +30,32 @@ public class Exam {
         this.score = score;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.subject);
+        hash = 29 * hash + Objects.hashCode(this.score);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Exam other = (Exam) obj;
+        if (!Objects.equals(this.subject, other.subject)) {
+            return false;
+        }
+        return Objects.equals(this.score, other.score);
+    }
+    
     @Override
     public String toString() {
         return "Exam{" + "subject=" + subject + ", score=" + score + '}';
--- src/main/java/com/ocp/day16_map/Student.java
+++ src/main/java/com/ocp/day16_map/Student.java
@@ -1,5 +1,7 @@
 package com.ocp.day16_map;
 
+import java.util.Objects;
+
 public class Student {
     private Integer id; // 學號
     private String name; // 姓名
@@ -28,6 +30,32 @@ public class Student {
         this.name = name;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        hash = 89 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+    
     @Override
     public String toString() {
         return "Student{" + "id=" + id + ", name=" + name + '}';
