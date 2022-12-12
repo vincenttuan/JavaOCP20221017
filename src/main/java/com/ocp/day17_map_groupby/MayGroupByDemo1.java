234c28df64f71541097001a5ae72a74c8fc131ba
teacher <teacher@BE-403->
2022-12-12 18:54:38 +0800

    ocp

--- src/main/java/com/ocp/day17_map_groupby/MayGroupByDemo1.java
+++ src/main/java/com/ocp/day17_map_groupby/MayGroupByDemo1.java
@@ -2,6 +2,9 @@ package com.ocp.day17_map_groupby;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class MayGroupByDemo1 {
     
@@ -9,6 +12,18 @@ public class MayGroupByDemo1 {
         
         List<String> fruits = 
                 Arrays.asList("apple", "apple", "banana", "orange", "orange", "apple", "papaya", "papaya", "watermelon");
+        
+        // 每一種水果有幾個
+        // {papaya=2, orange=2, banana=1, apple=3, watermelon=1}
+        Map<String, Long> result = fruits.stream()
+                                         .collect(
+                                             Collectors.groupingBy(
+                                                 //name -> name, Collectors.counting()
+                                                 Function.identity(), Collectors.counting()
+                                             )
+                                         );
+        
+        System.out.println(result);
         
     }
     
