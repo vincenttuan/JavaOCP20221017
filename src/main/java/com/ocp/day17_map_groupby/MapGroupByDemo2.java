bef38024e23bb704d24e1d949e4b948f0d1e8436
teacher <teacher@BE-403->
2022-12-12 19:31:55 +0800

    ocp

--- src/main/java/com/ocp/day17_map_groupby/MapGroupByDemo2.java
+++ src/main/java/com/ocp/day17_map_groupby/MapGroupByDemo2.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MapGroupByDemo2 {
@@ -37,12 +38,35 @@ public class MapGroupByDemo2 {
             1.99=[Fruit{name=banana, qty=20, price=1.99}, Fruit{name=banana, qty=10, price=1.99}], 
             0.99=[Fruit{name=apple, qty=10, price=0.99}, Fruit{name=apple, qty=20, price=0.99}, Fruit{name=apple, qty=10, price=0.99}, Fruit{name=apple, qty=10, price=0.99}], 
             2.99=[Fruit{name=orange, qty=10, price=2.99}, Fruit{name=watermelon, qty=10, price=2.99}], 
-            3.99=[Fruit{name=papaya, qty=10, price=3.99}, Fruit{name=papaya, qty=30, price=3.99}]}
-
+            3.99=[Fruit{name=papaya, qty=10, price=3.99}, Fruit{name=papaya, qty=30, price=3.99}]
+        }
         */
         Map<Double, List<Fruit>> groupByPrice = fruits.stream().collect(
             Collectors.groupingBy(Fruit::getPrice)
         );
         System.out.println(groupByPrice);
+        
+        // group by price
+        /*
+        list
+        {
+            1.99=banana, banana, 
+            0.99=apple, apple, apple, 
+            2.99=orange, watermelon,
+            3.99=papaya, papaya
+        }
+        透過 toSet() 剔除重複資料
+        {
+            1.99=[banana], 
+            0.99=[apple], 
+            2.99=[orange, watermelon], 
+            3.99=[papaya]
+        }
+        */
+        
+        Map<Double, Set<String>> groupByPrice2 = fruits.stream().collect(
+            Collectors.groupingBy(Fruit::getPrice, Collectors.mapping(Fruit::getName, Collectors.toSet()))
+        );
+        System.out.println(groupByPrice2);
     }
 }
