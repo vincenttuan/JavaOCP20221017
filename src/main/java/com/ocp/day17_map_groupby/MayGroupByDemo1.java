653f75408ca83b0887fd11b79d4a034961e1f11a
teacher <teacher@BE-403->
2022-12-12 18:47:03 +0800

    ocp

---
+++ src/main/java/com/ocp/day17_map_groupby/MayGroupByDemo1.java
@@ -0,0 +1,15 @@
+package com.ocp.day17_map_groupby;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MayGroupByDemo1 {
+    
+    public static void main(String[] args) {
+        
+        List<String> fruits = 
+                Arrays.asList("apple", "apple", "banana", "orange", "orange", "apple", "papaya", "papaya", "watermelon");
+        
+    }
+    
+}
