267db4ebad8500ee653838a314871dbe279e4bf7
teacher <teacher@BE-403->
2022-12-16 19:23:59 +0800

    ocp

--- src/main/java/com/ocp/day18_exception/ExceptionDemo3.java
+++ src/main/java/com/ocp/day18_exception/ExceptionDemo3.java
@@ -1,19 +1,19 @@
 package com.ocp.day18_exception;
 
-public class ExceptionDemo2 {
+public class ExceptionDemo3 {
     public static void main(String[] args) {
         try {
             printScore(-40);
-        } catch (RuntimeException e) {
-            System.out.printf("錯誤訊息: %s\n", e.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
         System.out.println("結束...");
     }
     
     // 印出 score 與是否及格
-    public static void printScore(int score) {
+    public static void printScore(int score) throws Exception {
         if (score < 0 || score > 100) {
-            RuntimeException re = new RuntimeException("分數範圍錯誤"); // 創造一個例外
+            Exception re = new Exception("分數範圍錯誤"); // 創造一個例外
             throw re; // 拋出例外
         }
         boolean isPass = score >= 60;
