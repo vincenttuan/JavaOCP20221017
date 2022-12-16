262a03208a2c62c0d4b4c25ea366aea361a18b56
teacher <teacher@BE-403->
2022-12-16 19:17:02 +0800

    ocp

---
+++ src/main/java/com/ocp/day18_exception/ExceptionDemo2.java
@@ -0,0 +1,25 @@
+package com.ocp.day18_exception;
+
+import org.w3c.dom.ranges.RangeException;
+
+public class ExceptionDemo2 {
+    public static void main(String[] args) {
+        try {
+            printScore(-40);
+        } catch (RuntimeException e) {
+            System.out.printf("錯誤訊息: %s\n", e.getMessage());
+        }
+        System.out.println("結束...");
+    }
+    
+    // 印出 score 與是否及格
+    public static void printScore(int score) {
+        if (score < 0 || score > 100) {
+            RuntimeException re = new RuntimeException("分數範圍錯誤"); // 創造一個例外
+            throw re; // 拋出例外
+        }
+        boolean isPass = score >= 60;
+        System.out.printf("%d %s\n", score, isPass?"及格":"不及格");
+        
+    }
+}
