db9ccc67490d3ac2b0cf31de64a6d32cdb00cc70
teacher <teacher@BE-403->
2022-12-19 21:41:24 +0800

    ocp

--- src/main/java/com/ocp/day19_jdbc/Select.java
+++ src/main/java/com/ocp/day19_jdbc/Select.java
@@ -4,8 +4,10 @@ package com.ocp.day19_jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-// SQL => SELECT c.CUSTOMER_ID, c."NAME", c.PHONE, c.EMAIL FROM CUSTOMER c;
+// SQL => SELECT c.CUSTOMER_ID, c."NAME", c.PHONE, c.EMAIL FROM CUSTOMER c
 public class Select {
     
     public static void main(String[] args) throws Exception {
@@ -17,6 +19,24 @@ public class Select {
         String user = "app";
         String password = "app";
         Connection conn = DriverManager.getConnection(url, user, password);
+        // 取得 Statement
+        Statement stmt = conn.createStatement();
+        // 撰寫 sql 並執行 executeQuery 後得到 ResultSet
+        String sql = "SELECT c.CUSTOMER_ID, c.NAME, c.PHONE, c.EMAIL FROM CUSTOMER c";
+        ResultSet rs = stmt.executeQuery(sql);
+        // 分析 rs (裡面存放的就是資料表的資訊)
+        while (rs.next()) { // 走訪集合資料
+            int id = rs.getInt("CUSTOMER_ID");
+            String name = rs.getString("NAME");
+            String phone = rs.getString("PHONE");
+            String email = rs.getString("EMAIL");
+            System.out.printf("| %5d | %-30s | %15s | %-30s |\n", id, name, phone, email);
+        }
+        
+        // 關閉資源
+        rs.close();
+        stmt.close();
+        conn.close();
         
     }
     
