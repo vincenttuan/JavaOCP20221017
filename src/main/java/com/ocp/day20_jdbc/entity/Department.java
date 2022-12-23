dce2ede1a7d9163ada80b5a71ab606c8210dc2b2
teacher <teacher@BE-403->
2022-12-23 19:34:08 +0800

    ocp

---
+++ src/main/java/com/ocp/day20_jdbc/entity/Department.java
@@ -0,0 +1,28 @@
+package com.ocp.day20_jdbc.entity;
+
+public class Department {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+}
