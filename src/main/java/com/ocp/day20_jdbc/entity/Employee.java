2800fa3d6d9e1f8380bfe0ac189003333d5d289f
teacher <teacher@BE-403->
2022-12-23 19:48:24 +0800

    ocp

---
+++ src/main/java/com/ocp/day20_jdbc/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.ocp.day20_jdbc.entity;
+
+public class Employee {
+    private Integer id;
+    private String name;
+    private Integer salary;
+    private Integer deptId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", name=" + name + ", salary=" + salary + ", deptId=" + deptId + '}';
+    }
+    
+    
+    
+}
