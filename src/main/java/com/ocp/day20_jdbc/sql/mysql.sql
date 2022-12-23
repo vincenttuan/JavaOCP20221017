5d30e9a1d50b689aa1b9bbcf6a7e88edeea80317
teacher <teacher@BE-403->
2022-12-23 19:27:08 +0800

    ocp

--- src/main/java/com/ocp/day20_jdbc/sql/mysql.sql
+++ src/main/java/com/ocp/day20_jdbc/sql/mysql.sql
@@ -12,9 +12,15 @@ create table employee(
     id int not null generated always as identity,
     name varchar(50) not null,
     salary int,
-    dept_id int,
+    dept_id int not null,
     primary key(id),
     foreign key(dept_id) references department(id)
 );
+-- 建立紀錄
+insert into department(name) values('IT');
+insert into department(name) values('Sales');
+insert into employee(name, salary, dept_id) values('John', 55000, 1);
+insert into employee(name, salary, dept_id) values('Mary', 65000, 2);
+insert into employee(name, salary, dept_id) values('Jack', 75000, 1);
 
 
