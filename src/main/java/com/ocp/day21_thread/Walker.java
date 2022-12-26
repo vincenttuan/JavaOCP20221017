3159ff528c8c507ac4cbd30951540149ffc6a59b
teacher <teacher@BE-403->
2022-12-26 19:38:03 +0800

    ocp

---
+++ src/main/java/com/ocp/day21_thread/Walker.java
@@ -0,0 +1,12 @@
+package com.ocp.day21_thread;
+
+public class Walker {
+    
+    public void job() {
+        String threadName = Thread.currentThread().getName(); // 取得執行緒的名字
+        for(int i=1;i<=1000;i++) {
+            System.out.printf("%s 走了 %d 步\n", threadName, i);
+        }
+    }
+    
+}
