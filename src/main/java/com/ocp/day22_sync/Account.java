6e596c8fa889c963f5c8c4957178212af76e3a56
teacher <teacher@BE-403->
2022-12-30 20:58:20 +0800

    ocp

---
+++ src/main/java/com/ocp/day22_sync/Account.java
@@ -0,0 +1,25 @@
+package com.ocp.day22_sync;
+
+public class Account {
+    
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
+    }
+    
+    public void withdraw(int amount) {
+        String threadName = Thread.currentThread().getName();
+        System.out.printf("%s 提款 $%d 目前餘額: $%d\n", threadName, amount, balance);
+        if(balance >= amount) {
+            for(int i=0;i<Integer.MAX_VALUE;i++); // 模擬提款的運作時間
+            // 減去提款金額
+            balance = balance - amount;
+            System.out.printf("%s 提款 $%d (成功) 目前餘額: $%d\n", threadName, amount, balance);
+        } else {
+            System.out.printf("%s 提款 $%d (餘額不足) 目前餘額: $%d\n", threadName, amount, balance);
+        }
+    }
+    
+    
+}
