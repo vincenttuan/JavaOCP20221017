ae6a8f164fe6c8594b4e3b5c7d6f6d5761df25b5
teacher <teacher@BE-403->
2023-01-06 21:24:17 +0800

    ocp

--- src/main/java/com/ocp/day23_thread/LottoDemo.java
+++ src/main/java/com/ocp/day23_thread/LottoDemo.java
@@ -6,11 +6,10 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class LottoDemo {
     public static void main(String[] args) {
         ThreadPoolExecutor exec = (ThreadPoolExecutor)Executors.newCachedThreadPool();
-        exec.submit(new Lotto());
-        exec.submit(new Lotto());
-        exec.submit(new Lotto());
-        exec.submit(new Lotto());
-        exec.submit(new Lotto());
+        int i = 100;
+        while( i-- > 0) {
+            exec.submit(new Lotto());
+        }
         System.out.println("Thread count: " + Thread.activeCount());
         // 關閉服務
         exec.shutdown();
