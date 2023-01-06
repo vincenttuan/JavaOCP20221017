5d87442e0a79beb2e85430b16caf5bf4e0ca7964
teacher <teacher@BE-403->
2023-01-06 21:25:43 +0800

    ocp

--- src/main/java/com/ocp/day23_thread/LottoDemo.java
+++ src/main/java/com/ocp/day23_thread/LottoDemo.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class LottoDemo {
     public static void main(String[] args) {
-        ThreadPoolExecutor exec = (ThreadPoolExecutor)Executors.newCachedThreadPool();
+        //ThreadPoolExecutor exec = (ThreadPoolExecutor)Executors.newCachedThreadPool();
+        ThreadPoolExecutor exec = (ThreadPoolExecutor)Executors.newFixedThreadPool(2);
         int i = 100;
         while( i-- > 0) {
             exec.submit(new Lotto());
