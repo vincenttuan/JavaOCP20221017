d783d5b18daa4adefa1a667ae1a5f0203911328e
teacher <teacher@BE-403->
2023-01-09 21:19:13 +0800

    ocp

--- src/main/java/com/ocp/day24_thread/SumTaskDemo.java
+++ src/main/java/com/ocp/day24_thread/SumTaskDemo.java
@@ -10,19 +10,27 @@ public class SumTaskDemo {
         int[] array = new int[len];
         // 期望結果
         int expectedSum = 0;
+        // 時間
+        long startTime, endTime;
         for(int i=0;i<array.length;i++) {
             array[i] = i + 1;
+        }
+        // 一般計算
+        startTime = System.currentTimeMillis();
+        for(int i=0;i<array.length;i++) {
             expectedSum += array[i];
         }
+        endTime = System.currentTimeMillis();
         System.out.println(Arrays.toString(array));
         System.out.printf("期望結果: %d\n", expectedSum);
+        System.out.printf("執行時間: %dms\n", (endTime - startTime));
         System.out.println("--------------------------------");
         // forkjoin:
         ForkJoinTask<Integer> task = new SumTask(array, 0, array.length);
         // startTime
-        long startTime = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
         int result = ForkJoinPool.commonPool().invoke(task); // 
-        long endTime = System.currentTimeMillis();
+        endTime = System.currentTimeMillis();
         System.out.printf("執行結果(forkjoin): %d\n", result);
         System.out.printf("執行時間(forkjoin): %dms\n", (endTime - startTime));
     }
