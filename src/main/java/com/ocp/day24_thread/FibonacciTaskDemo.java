dcbd642cbd273625031e3b0e4a95a26e78c606a6
teacher <teacher@BE-403->
2023-01-09 21:44:28 +0800

    ocp

--- src/main/java/com/ocp/day24_thread/FibonacciTaskDemo.java
+++ src/main/java/com/ocp/day24_thread/FibonacciTaskDemo.java
@@ -1,5 +1,8 @@
 package com.ocp.day24_thread;
 
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
 public class FibonacciTaskDemo {
     public static void main(String[] args) {
         int num = 40;
@@ -9,5 +12,12 @@ public class FibonacciTaskDemo {
         long result = new Fibonacci().fib(num);
         endTime = System.currentTimeMillis();
         System.out.printf("一般方法: 第 %d 項 = %,d 花費時間: %dms\n", num, result, (endTime-startTime));
+        // forkjoin
+        ForkJoinTask<Long> task = new FibonacciTask(num);
+        startTime = System.currentTimeMillis();
+        long result2 = new ForkJoinPool(4).invoke(task);
+        endTime = System.currentTimeMillis();
+        System.out.printf("forkjoin方法: 第 %d 項 = %,d 花費時間: %dms\n", num, result2, (endTime-startTime));
+        
     }
 }
