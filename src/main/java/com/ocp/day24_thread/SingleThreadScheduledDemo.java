b281db4bae28e517111e439ec4a8f35984891a84
teacher <teacher@BE-403->
2023-01-09 19:03:41 +0800

    ocp

--- src/main/java/com/ocp/day24_thread/SingleThreadScheduledDemo.java
+++ src/main/java/com/ocp/day24_thread/SingleThreadScheduledDemo.java
@@ -1,11 +1,22 @@
 package com.ocp.day24_thread;
 
 // 單次性排程
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public class SingleThreadScheduledDemo {
-    public static void main(String[] args) {
-        
-        
-        
+    public static void main(String[] args) throws ExecutionException, InterruptedException{
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        System.out.println("停一秒後開始運作: " + new Date());
+        ScheduledFuture future = exec.schedule(new Lotto(), 1, TimeUnit.SECONDS);
+        System.out.println(future.get());
+        System.out.println("結束運作: " + new Date());
+        exec.shutdown();
     }
             
 }
