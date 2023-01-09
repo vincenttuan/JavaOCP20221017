5b4395f0a7e3963e2e36609051b1195f2ce7de3b
teacher <teacher@BE-403->
2023-01-09 21:16:59 +0800

    ocp

--- src/main/java/com/ocp/day24_thread/SumTaskDemo.java
+++ src/main/java/com/ocp/day24_thread/SumTaskDemo.java
@@ -19,7 +19,11 @@ public class SumTaskDemo {
         System.out.println("--------------------------------");
         // forkjoin:
         ForkJoinTask<Integer> task = new SumTask(array, 0, array.length);
+        // startTime
+        long startTime = System.currentTimeMillis();
         int result = ForkJoinPool.commonPool().invoke(task); // 
+        long endTime = System.currentTimeMillis();
         System.out.printf("執行結果(forkjoin): %d\n", result);
+        System.out.printf("執行時間(forkjoin): %dms\n", (endTime - startTime));
     }
 }
