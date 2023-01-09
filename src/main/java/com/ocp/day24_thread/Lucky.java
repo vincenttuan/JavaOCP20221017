70a5c0655648d05edff9969d666e3115b84527d7
teacher <teacher@BE-403->
2023-01-09 19:06:09 +0800

    ocp

---
+++ src/main/java/com/ocp/day24_thread/Lucky.java
@@ -0,0 +1,16 @@
+package com.ocp.day24_thread;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class Lucky implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws InterruptedException {
+        //Thread.sleep(new Random().nextInt(10));
+        Thread.sleep(500);
+        int n = new Random().nextInt(100);
+        return n;
+    }
+    
+}
