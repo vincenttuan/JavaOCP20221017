d9f6f9934e31daf27b86afd8ed5d8f20d101b8b6
teacher <teacher@BE-403->
2023-01-09 19:32:01 +0800

    ocp

--- src/main/java/com/ocp/day24_thread/Lotto.java
+++ src/main/java/com/ocp/day24_thread/Lotto.java
@@ -1,18 +1,21 @@
 package com.ocp.day24_thread;
 
+import java.util.Date;
 import java.util.Random;
 
 public class Lotto implements Runnable {
 
     @Override
     public void run() {
+        System.out.print(new Date() + " [ ");
         try {
             //Thread.sleep(new Random().nextInt(10));
             Thread.sleep(500);
         } catch (InterruptedException ex) {
         }
         int n = new Random().nextInt(100);
-        System.out.println(n);
+        System.out.print(n);
+        System.out.println(" ] " + new Date());
     }
     
 }
--- src/main/java/com/ocp/day24_thread/ScheduledFixedDemo.java
+++ src/main/java/com/ocp/day24_thread/ScheduledFixedDemo.java
@@ -1,7 +1,16 @@
 package com.ocp.day24_thread;
 
 // scheduleWithFixedDelay
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 // scheduleAtFixedRate
 public class ScheduledFixedDemo {
-    
+    public static void main(String[] args) {
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleWithFixedDelay(new Lotto(), 1, 1, TimeUnit.SECONDS);
+        System.out.println("執行中...");
+    }
 }
