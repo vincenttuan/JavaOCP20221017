1bc02a56498d00ce2ef20ef2d4087183619088ce
teacher <teacher@BE-403->
2023-01-09 19:51:34 +0800

    ocp

---
+++ src/main/java/com/ocp/day24_thread/InvokeAllDemo.java
@@ -0,0 +1,33 @@
+package com.ocp.day24_thread;
+
+// 連續取得 5 個幸運數字
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class InvokeAllDemo {
+    
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService service = Executors.newCachedThreadPool();
+        Collection<Callable<Integer>> list = new ArrayList<>();
+        list.add(new Lucky());
+        list.add(new Lucky());
+        list.add(new Lucky());
+        list.add(new Lucky());
+        list.add(new Lucky());
+        
+        List<Future<Integer>> results = service.invokeAll(list);
+        for(Future future : results) {
+            System.out.println(future.get());
+        }
+        service.shutdown();
+        
+    }
+    
+}
