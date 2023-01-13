df88923248a4bcfe4948088d01ecfda98956418f
teacher <teacher@BE-403->
2023-01-13 20:45:35 +0800

    ocp

--- src/main/java/com/ocp/day25_io/FileReadeDemo.java
+++ src/main/java/com/ocp/day25_io/FileReadeDemo.java
@@ -11,6 +11,13 @@ public class FileReadeDemo {
             StringBuilder sb = new StringBuilder();
             while (fr.read(buffer) != -1) {                
                 //System.out.print(buffer[0]);
+                if(buffer[0] == '\n' || buffer[0] == '[') {
+                    continue;
+                }
+                if(buffer[0] == ']') {
+                    sb.append(", ");
+                    continue;
+                }
                 sb.append(buffer);
             }
             System.out.println(sb);
