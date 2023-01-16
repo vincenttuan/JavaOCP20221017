9f4ca61e50ea1f9822bb136bf7dc11a83e9c6d35
teacher <teacher@BE-403->
2023-01-16 21:25:47 +0800

    ocp

---
+++ src/main/java/com/ocp/day26_io/Java11ReadAndWrite.java
@@ -0,0 +1,20 @@
+package com.ocp.day26_io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Java11ReadAndWrite {
+    public static void main(String[] args) throws IOException {
+        String str = "Hello";
+        String fileName = "src/main/java/com/ocp/day26_io/demo.txt";
+        Path path = Paths.get(fileName);
+        Files.writeString(Files.createFile(path), str, StandardOpenOption.WRITE);
+        System.out.println(str + " written to " + path.toAbsolutePath());
+        String s = Files.readString(path);
+        System.out.println("read: " + s);
+
+    }
+}
