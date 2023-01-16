1b3c236f32401535e41b2e3899c34734b4fb837c
teacher <teacher@BE-403->
2023-01-16 20:49:17 +0800

    ocp

---
+++ src/main/java/com/ocp/day26_io/FilesCopyAndDelete.java
@@ -0,0 +1,19 @@
+package com.ocp.day26_io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FilesCopyAndDelete {
+    public static void main(String[] args) throws IOException{
+        Path source = Paths.get("src/main/java/com/ocp/day25_io/data/boss.png");
+        Path dest = Paths.get("src/main/java/com/ocp/day26_io/boss.png");
+        
+        Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+        
+        System.out.println("COPY OK!");
+        
+    }
+}
