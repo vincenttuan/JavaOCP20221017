c54133f6708ad5c4da3ed8c94908df670751b301
teacher <teacher@BE-403->
2023-01-16 21:00:23 +0800

    ocp

---
+++ src/main/java/com/ocp/day26_io/FilesStreamCopy.java
@@ -0,0 +1,19 @@
+package com.ocp.day26_io;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FilesStreamCopy {
+    public static void main(String[] args) throws IOException {
+        // SAKURA
+        String url = "http://img.touxiangwu.com/uploads/allimg/2021082516/c22fn0qzosm.jpg";
+        URI uri = URI.create(url);
+        Path dest = Paths.get("src/main/java/com/ocp/day26_io/SAKURA.png");
+        Files.copy(uri.toURL().openStream(), dest, StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("COPY Image OK!");
+    }
+}
